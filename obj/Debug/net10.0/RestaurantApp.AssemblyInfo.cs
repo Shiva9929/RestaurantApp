32ecffd2d96f84bca509a92b2e988da4ff7ec1b4
Shiva Shankar <spalashiva9929@gmail.com>
2026-06-06 01:12:00 -0400

    Fix UTC datetime in seed data

--- obj/Debug/net10.0/RestaurantApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/RestaurantApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6527123644f2b087d3e664e686cd1b2509590f8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d494e488fd3fb7b19a6274a8f88138c7884ac5c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
