f1f568ba9d7565043da0e01631689ef10833c991
Shiva Shankar <spalashiva9929@gmail.com>
2026-06-05 21:12:14 -0400

    Add stylish frontend HTML with menu, orders and feedback

--- obj/Debug/net10.0/RestaurantApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/RestaurantApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d22bc1e923058e656ff826c7aed1702667c16f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
